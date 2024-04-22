abf80f91f1de8a45bb4144a280ecd467bc8ff1d1
Thanoux1204 <ehouy92380@gmail.com>
2024-04-22 22:49:03 +0200

    commentaires

--- src/Main.java
+++ src/Main.java
@@ -13,36 +13,38 @@ public class Main {
         lire("src/in/data.txt", valeurs);
         printVal(valeurs);
         trier(valeurs);
-        //valeurs = valeurs.stream().distinct().collect(Collectors.toList());
+        /** Supprimer les doublons */
+        valeurs = valeurs.stream().distinct().collect(Collectors.toList());
         printVal(valeurs);
+        /** Test 1 */
         String filePath = "src/out/output.txt";
+        /** Test 2 */
         String filePath2 = "src/out/output2.txt";
+        /** Test 3 */
         String filePath3 = "src/out/output3.txt";
         ecrire(filePath3,valeurs);
 
 
     }
     public static void trier(List<Integer> valeurs){
+        /** Trier dans l'ordre décroissant */
         Collections.sort(valeurs, Collections.reverseOrder());
-
     }
     public static void lire(String nomFichier, List<Integer> valeurs) throws FileNotFoundException {
         Scanner fichier =new Scanner(new FileInputStream(nomFichier));
-        int compteur = 0;
         while(fichier.hasNextInt()){
             Scanner ligne = new Scanner(fichier.nextLine());
             valeurs.add(ligne.nextInt());
             ligne.close();
         }
         fichier.close();
-        System.out.println(compteur);
     }
     public static void ecrire(String filePath, List<Integer> valeurs){
         // Écriture des valeurs dans le fichier
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             // Écrire les valeurs dans le fichier avec un espace entre chaque valeur
             for (Integer valeur : valeurs) {
-                writer.write(valeur + "\n");
+                writer.write(valeur + " ");
             }
         } catch (IOException e) {
             e.printStackTrace();
